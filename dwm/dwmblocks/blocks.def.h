e20f190a1b54558cedae4bf72b4d7592b329200a
Duy Pham <cunbidun@gmail.com>
2021-07-05 21:50:15 -0400

    Add cpu to dwmblock

--- dwm/dwmblocks/blocks.def.h
+++ dwm/dwmblocks/blocks.def.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-    {"MEM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 20, 0},
-    {"NET: ", "get_internet", 30, 0},
-    {"DOGE: ", "get_doge", 30, 0},
+    {"CPU: ", "get_cpu", 10, 0},
+    {"MEM: ", "get_memory", 10, 0},
+    {"NET: ", "get_internet", 10, 0},
+    {"DOGE: ", "get_doge", 10, 0},
     {"BAT: ", "get_battery", 10, 0},
     {"VOL: " , "get_volume", 30, 10},
     {"TEMP: ", "get_temp", 10, 0},
