c44aac4bfb85fd2355f039e51041e3ea3fb8aca9
Duy Pham <cunbidun@gmail.com>
2021-11-18 16:39:43 -0500

    Add Dwm async statusbar

---
+++ dwm/dwmblocks-async/config.h
@@ -0,0 +1,16 @@
+#define CMDLENGTH 50
+#define DELIMITER " │ "
+// #define LEADING_DELIMITER
+// #define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+  BLOCK("get_cpu",                      10,                   0)
+  BLOCK("get_memory",                   10,                   0)
+  BLOCK("get_internet",                 10,                   0)
+  BLOCK("get_doge",                     10,                   0)
+  BLOCK("get_battery",                  5,                   12)
+  BLOCK("get_volume",                   30,                  10)
+  BLOCK("get_temp",                     10,                   0)
+  BLOCK("get_language",                 30,                  11)
+	BLOCK("date '+%a %b %d, %H:%M:%S'",    1,                   0)
+};
--- dwm/dwmblocks/blocks.def.h
+++ dwm/dwmblocks/blocks.def.h
@@ -1,16 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-    {"CPU: ", "get_cpu", 10, 0},
-    {"MEM: ", "get_memory", 10, 0},
-    {"NET: ", "get_internet", 10, 0},
-    {"DOGE: ", "get_doge", 10, 0},
-    // {"MM: ", "mm", 10, 0},
-    {"BAT: ", "get_battery", 5, 12},
-    {"VOL: " , "get_volume", 30, 10},
-    {"TEMP: ", "get_temp", 10, 0},
-    {"LANG: " , "get_language", 30, 11},
-    {"", "date '+%a %b %d, %H:%M:%S'", 1, 0},
+    /*Icon*/          /*Command*/                     /*Update Interval*/ /*Update Signal*/
+    {"",  "get_cpu",                     10,                   0},
+    {"",  "get_memory",                  10,                   0},
+    {"",  "get_internet",                10,                   0},
+    {"",  "get_doge",                    10,                   0},
+    // {"\x09MM: ",   "mm", 10, 0},
+    {"",  "get_battery",                   5,                   12},
+    {"",  "get_volume",                   30,                  10},
+    {"",  "get_temp",                     10,                   0},
+    {"",  "get_language",                 30,                  11},
+    {"",  "date '+%a %b %d, %H:%M:%S'",    1,                    0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
