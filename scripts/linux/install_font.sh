45f80444f1b9a91f742cb80c7459881838446633
Duy Pham <cunbidun@gmail.com>
2022-05-15 09:34:13 -0500

    Config dmenu

---
+++ scripts/linux/install_font.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+yay -S adobe-source-code-pro-fonts ttf-weather-icons
+yay -S adobe-source-han-sans-kr-fonts ttf-baekmuk # korean font
+yay -S apple-fonts                                # apple font
+yay -S adobe-base-14-fonts
+yay -S nerd-fonts-complete
+yay -S noto-fonts noto-fonts-cjk noto-fonts-emoji
+yay -S all-repository-fonts
