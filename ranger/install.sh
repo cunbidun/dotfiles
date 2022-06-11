629afed3debe720eca16cda56e2d0131753c41a3
Duy Pham <cunbidun@gmail.com>
2022-06-11 11:59:45 -0500

    Update zsh

--- ranger/install.sh
+++ ranger/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# symlink config
+stow common --no-folding -t "$HOME"
+
 # Install ranger_devicons
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
