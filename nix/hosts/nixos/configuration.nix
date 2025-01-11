185d99ca0ad0b4310e4fd0999dae69e5db94f6dc
Duy Pham <cunbidun@gmail.com>
2025-01-11 00:34:04 -0500

    update

--- nix/hosts/nixos/configuration.nix
+++ nix/hosts/nixos/configuration.nix
@@ -99,7 +99,7 @@
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    portalPackage = pkgs.xdg-desktop-portal-hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
     withUWSM = true;
   };
   programs.uwsm.enable = true;
