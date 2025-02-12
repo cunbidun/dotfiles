090e670c48173af5b28c87a8f59110c0a5d32e7f
Duy Pham <cunbidun@gmail.com>
2025-02-12 12:55:29 -0800

    Update

--- nix/home-manager/configs/vscode.nix
+++ nix/home-manager/configs/vscode.nix
@@ -8,6 +8,103 @@ in {
   programs.vscode = {
     enable = true;
 
+    keybindings = [
+      {
+        key = "ctrl+h";
+        command = "workbench.action.navigateLeft";
+      }
+      {
+        key = "ctrl+j";
+        command = "workbench.action.navigateDown";
+      }
+      {
+        key = "ctrl+k";
+        command = "workbench.action.navigateUp";
+      }
+      {
+        key = "ctrl+l";
+        command = "workbench.action.navigateRight";
+      }
+
+      # When the explorer is focused (and no input field is active),
+      # pressing "o" will open the file (or toggle a folder’s expansion)
+      {
+        key = "o";
+        command = "list.select";
+        when = "filesExplorerFocus && foldersViewVisible && !inputFocus";
+      }
+      # Pressing "r" will invoke the rename action.
+      {
+        key = "r";
+        command = "renameFile";
+        when = "filesExplorerFocus && foldersViewVisible && !explorerResourceIsRoot && !explorerResourceReadonly && !inputFocus";
+      }
+      # Pressing "v" will open the selected file in a vertical split.
+      {
+        key = "v";
+        command = "workbench.action.splitEditor";
+        when = "filesExplorerFocus && foldersViewVisible && !inputFocus";
+      }
+      # Open in horizontal split
+      {
+        key = "s";
+        command = "workbench.action.splitEditorDown";
+        when = "filesExplorerFocus && foldersViewVisible && !inputFocus";
+      }
+      # Delete file or folder
+      {
+        key = "d";
+        command = "deleteFile";
+        when = "filesExplorerFocus && foldersViewVisiblee && !explorerResourceMoveableToTrash && !explorerResourceReadonly && !inputFocus";
+      }
+      # Copy file with "c"
+      {
+        key = "c";
+        command = "filesExplorer.copy";
+        when = "filesExplorerFocus && foldersViewVisible && !inputFocus";
+      }
+      # Cut file with "x" (note: the correct command is "filesExplorer.cut")
+      {
+        key = "x";
+        command = "filesExplorer.cut";
+        when = "filesExplorerFocus && foldersViewVisible && !inputFocus";
+      }
+      # Paste file with "p"
+      {
+        key = "p";
+        command = "filesExplorer.paste";
+        when = "filesExplorerFocus && foldersViewVisible && !inputFocus";
+      }
+      # Create new file (nvim-tree uses "a")
+      {
+        key = "a";
+        command = "explorer.newFile";
+        when = "filesExplorerFocus && !inputFocus";
+      }
+      {
+        key = "u";
+        command = "list.focusParent";
+        when = "filesExplorerFocus && foldersViewVisible && !inputFocus";
+      }
+      # Refresh explorer
+      {
+        key = "shift+r";
+        command = "workbench.files.action.refreshFilesExplorer";
+        when = "filesExplorerFocus && foldersViewVisible && !inputFocus";
+      }
+
+      {
+        "key" = "alt+cmd+j";
+        "command" = "editor.action.insertCursorBelow";
+        "when" = "editorTextFocus";
+      }
+
+      {
+        "key" = "alt+cmd+k";
+        "command" = "editor.action.insertCursorAbove";
+        "when" = "editorTextFocus";
+      }
+    ];
     userSettings = {
       "editor.minimap.enabled" = false;
       "editor.fontSize" = 13;
@@ -29,16 +126,36 @@ in {
         "<C-p>" = false;
         "<C-n>" = false;
       };
+      "vim.easymotion" = true;
+      "vim.easymotionMarkerBackgroundColor" = "#7e57c2";
+      "vim.leader" = " ";
+      # Define a non-recursive normal-mode mapping: pressing <leader> then e runs
+      # the command to toggle the sidebar (which in this case is used to show or hide the Explorer)
+      "vim.normalModeKeyBindingsNonRecursive" = [
+        {
+          before = ["<leader>" "e"];
+          commands = ["workbench.action.toggleSidebarVisibility"];
+        }
+      ];
       "vim.normalModeKeyBindings" = [
         {
           before = ["<TAB>"];
-          commands = ["workbench.action.nextEditor"];
+          commands = ["workbench.action.nextEditorInGroup"];
           silent = true;
         }
         {
           before = ["<S-TAB>"];
-          commands = ["workbench.action.previousEditor"];
+          commands = ["workbench.action.previousEditorInGroup"];
           silent = true;
+        }
+        {
+          before = ["<S-x>"];
+          commands = ["workbench.action.closeActiveEditor"];
+          silent = true;
+        }
+        {
+          before = ["<leader>" "m"];
+          after = ["<leader>" "<leader>"];
         }
       ];
       "vim.useSystemClipboard" = true;
