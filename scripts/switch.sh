1dd9e0bbec95a67dfc2efd1e4d01c990aeca84c6
Duy Pham <cunbidun@gmail.com>
2025-03-30 10:28:12 -0400

    Auto commit: 2025-03-30 10:28:12

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Detect OS and switch accordingly
+os=$(uname)
+
 # get the directory of the script
 script_dir=$(dirname "$(readlink -f "$0")")
 
@@ -24,28 +27,30 @@ if [ -z "$git_root" ]; then
   exit 1
 fi
 
+if [ "$os" != "Darwin" ]; then
+  # check if nix/hosts/nixos/hardware-configuration.nix content is similar to /etc/nixos/hardware-configuration.nix
+  # if not, copy the content from /etc/nixos/hardware-configuration.nix to nix/hosts/nixos/hardware-configuration.nix
+  # this assume that /etc/nixos/hardware-configuration.nix is the source of truth for hardware configuration
+
+  # check if the file /etc/nixos/hardware-configuration.nix exists, if not print an error message and exit
+  if [ ! -f /etc/nixos/hardware-configuration.nix ]; then
+    echo "Error: /etc/nixos/hardware-configuration.nix does not exist."
+    exit 1
+  fi
+
+  # Compare the content of the two files
+  if ! cmp -s /etc/nixos/hardware-configuration.nix "$git_root/nix/hosts/nixos/hardware-configuration.nix"; then
+    echo "Updating nix/hosts/nixos/hardware-configuration.nix from /etc/nixos/hardware-configuration.nix"
+    # Copy the content from /etc/nixos/hardware-configuration.nix to nix/hosts/nixos/hardware-configuration.nix
+    cp /etc/nixos/hardware-configuration.nix "$git_root/nix/hosts/nixos/hardware-configuration.nix"
+  else
+    echo "No changes detected in hardware-configuration.nix."
+  fi
+fi
+
 # Change to the git repository root directory
 cd "$git_root"
 echo "Switching NixOS configuration from the git repository at: $git_root"
-
-# check if nix/hosts/nixos/hardware-configuration.nix content is similar to /etc/nixos/hardware-configuration.nix
-# if not, copy the content from /etc/nixos/hardware-configuration.nix to nix/hosts/nixos/hardware-configuration.nix
-# this assume that /etc/nixos/hardware-configuration.nix is the source of truth for hardware configuration
-
-# check if the file /etc/nixos/hardware-configuration.nix exists, if not print an error message and exit
-if [ ! -f /etc/nixos/hardware-configuration.nix ]; then
-  echo "Error: /etc/nixos/hardware-configuration.nix does not exist."
-  exit 1
-fi
-
-# Compare the content of the two files
-if ! cmp -s /etc/nixos/hardware-configuration.nix "$git_root/nix/hosts/nixos/hardware-configuration.nix"; then
-  echo "Updating nix/hosts/nixos/hardware-configuration.nix from /etc/nixos/hardware-configuration.nix"
-  # Copy the content from /etc/nixos/hardware-configuration.nix to nix/hosts/nixos/hardware-configuration.nix
-  cp /etc/nixos/hardware-configuration.nix "$git_root/nix/hosts/nixos/hardware-configuration.nix"
-else
-  echo "No changes detected in hardware-configuration.nix."
-fi
 
 # Auto-add all changes and commit if there are any
 git add -A
@@ -57,8 +62,6 @@ else
   echo "No changes to commit."
 fi
 
-# Detect OS and switch accordingly
-os=$(uname)
 if [ "$os" = "Darwin" ]; then
   echo "Detected macOS; running darwin-rebuild switch..."
   darwin-rebuild switch --flake ~/dotfiles#macbook-m1
