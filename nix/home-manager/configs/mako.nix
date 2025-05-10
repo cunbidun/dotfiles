cf6a0748b7c2ac7f4b139624f583afe02688ffbb
Duy Pham <cunbidun@gmail.com>
2025-05-10 11:44:17 -0400

    Update mako configs

--- nix/home-manager/configs/mako.nix
+++ nix/home-manager/configs/mako.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{...}: {
   services.mako = {
     enable = true;
 
@@ -29,3 +24,32 @@
     '';
   };
 }
+# {...}: {
+#   services.mako = {
+#     enable = true;
+#
+#     settings = {
+#       width = 300;
+#       height = 100;
+#       margin = "20";
+#       padding = "20";
+#       borderSize = 2;
+#       borderRadius = 0;
+#
+#       # Notification behavior
+#       defaultTimeout = 5000;
+#       ignoreTimeout = true;
+#       maxVisible = 20;
+#
+#       # Positioning and layout
+#       anchor = "top-right";
+#       layer = "overlay";
+#     };
+#     criteria = {
+#       "urgency=critical" = {
+#         "ignore-timeout" = 1;
+#       };
+#     };
+#   };
+# }
+
