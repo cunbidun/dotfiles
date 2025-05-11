fdce98119b6afed359b9afe0690c7dc98a9d8144
Duy Pham <cunbidun@gmail.com>
2025-05-11 18:23:43 -0400

    Update

--- flake.nix
+++ flake.nix
@@ -5,15 +5,8 @@
     nixpkgs-unstable = {url = "github:nixos/nixpkgs/nixos-unstable";};
     nixpkgs-stable = {url = "github:nixos/nixpkgs/nixos-24.11";};
     nix-darwin = {url = "github:LnL7/nix-darwin";};
-    home-manager = {
-      url = "github:nix-community/home-manager";
-    };
-    xremap-flake = {
-      url = "github:xremap/nix-flake";
-      inputs.hyprland.follows = "hyprland";
-    };
+    home-manager = {url = "github:nix-community/home-manager";};
     apple-fonts = {url = "github:Lyndeno/apple-fonts.nix";};
-    yazi.url = "github:sxyazi/yazi/v25.4.8";
 
     # +----------+
     # | Hyprland |
@@ -31,10 +24,15 @@
       url = "github:MartinLoeper/hyprfocus";
       inputs.hyprland.follows = "hyprland";
     };
+    xremap-flake = {
+      url = "github:xremap/nix-flake";
+      inputs.hyprland.follows = "hyprland";
+    };
 
     # +--------+
     # | Others |
     # +--------+
+    yazi = {url = "github:sxyazi/yazi/v25.4.8";};
     stylix = {url = "github:danth/stylix";};
     spicetify-nix = {url = "github:Gerg-L/spicetify-nix";};
 
@@ -89,61 +87,67 @@
         };
       };
     };
-  in {
-    ##########################
-    # macbook configurations #
-    ##########################
-    # nix --log-format internal-json run nix-darwin -- switch --flake ~/dotfiles#macbook-m1 |& nom
-    darwinConfigurations."macbook-m1" = nix-darwin.lib.darwinSystem {
-      pkgs = mkPkgs "aarch64-darwin";
-      specialArgs = {
-        inherit inputs;
-        stateVersion = 4;
-        userdata = userdata;
-      };
-      modules = [
-        inputs.mac-app-util.darwinModules.default
-        ./nix/hosts/macbook/configuration.nix
-        home-manager.darwinModules.home-manager
-        (mkHomeManagerModule "${project_root}/nix/hosts/macbook/home.nix")
-      ];
-    };
 
-    ##########################
-    # macbook configurations #
-    ##########################
-    # nix --log-format internal-json run nix-darwin -- switch --flake ~/dotfiles#macbook-intel |& nom
-    darwinConfigurations."macbook-intel" = nix-darwin.lib.darwinSystem {
-      pkgs = mkPkgs "x86_64-darwin";
-      specialArgs = {
-        inherit inputs;
-        stateVersion = 5;
-        userdata = userdata;
-      };
-      modules = [
-        inputs.mac-app-util.darwinModules.default
-        ./nix/hosts/macbook/configuration.nix
-        home-manager.darwinModules.home-manager
-        (mkHomeManagerModule "${project_root}/nix/hosts/macbook/home.nix")
-      ];
-    };
-
-    #######################
-    # nixos configuration #
-    #######################
-    nixosConfigurations = {
-      # build with sudo nixos-rebuild switch --flake ~/dotfiles#nixos
-      nixos = nixpkgs-unstable.lib.nixosSystem {
-        pkgs = mkPkgs "x86_64-linux";
+    mkDarwinSystem = {
+      system,
+      stateVersionNum,
+    }:
+      nix-darwin.lib.darwinSystem {
+        pkgs = mkPkgs system;
         specialArgs = {
-          inherit inputs;
-          userdata = userdata;
+          inherit inputs userdata;
+          stateVersion = stateVersionNum;
         };
         modules = [
-          ./nix/hosts/nixos/configuration.nix
-          home-manager.nixosModules.home-manager
-          (mkHomeManagerModule "${project_root}/nix/hosts/nixos/home.nix")
+          inputs.mac-app-util.darwinModules.default
+          ./nix/hosts/macbook/configuration.nix
+          home-manager.darwinModules.home-manager
+          (mkHomeManagerModule "${project_root}/nix/hosts/macbook/home.nix")
         ];
+      };
+
+    mkNixosHost = {
+      system,
+      hostPath,
+      homePath,
+    }:
+      nixpkgs-unstable.lib.nixosSystem {
+        pkgs = mkPkgs system;
+        specialArgs = {
+          inherit inputs userdata;
+        };
+        modules = [
+          hostPath
+          home-manager.nixosModules.home-manager
+          (mkHomeManagerModule homePath)
+        ];
+      };
+  in {
+    # -----------------------#
+    # macbook configurations #
+    # -----------------------#
+    darwinConfigurations = {
+      # nix --log-format internal-json run nix-darwin -- switch --flake ~/dotfiles#macbook-m1 |& nom
+      "macbook-m1" = mkDarwinSystem {
+        system = "aarch64-darwin";
+        stateVersionNum = 4;
+      };
+      # nix --log-format internal-json run nix-darwin -- switch --flake ~/dotfiles#macbook-intel |& nom
+      "macbook-intel" = mkDarwinSystem {
+        system = "x86_64-darwin";
+        stateVersionNum = 5;
+      };
+    };
+
+    # -----------------------#
+    #  nixos configurations  #
+    # -----------------------#
+    nixosConfigurations = {
+      # sudo nixos-rebuild switch --flake ~/dotfiles#nixos
+      nixos = mkNixosHost {
+        system = "x86_64-linux";
+        hostPath = ./nix/hosts/nixos/configuration.nix;
+        homePath = "${project_root}/nix/hosts/nixos/home.nix";
       };
     };
   };
