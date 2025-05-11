a008eedcadf783c3a611d75ba4e6c142d40e1965
Duy Pham <cunbidun@gmail.com>
2025-05-11 17:23:39 -0400

    Update

--- flake.nix
+++ flake.nix
@@ -71,6 +71,24 @@
         inherit system;
         overlays = [
           inputs.nur.overlays.default
+          (
+            final: prev: {
+              nur.repos.rycee.firefox-addons =
+                prev.nur.repos.rycee.firefox-addons
+                // {
+                  carrot = prev.nur.repos.rycee.firefox-addons.buildFirefoxXpiAddon rec {
+                    pname = "carrot";
+                    version = "0.6.8";
+                    url = "https://addons.mozilla.org/firefox/downloads/file/4450645/carrot-${version}.xpi";
+                    sha256 = "sha256-4QXp97JyqBBHcZbCReEzKCltTBc8WShETjVXYjq2WZ0";
+                    addonId = "{f0eeb71a-e5d6-48e6-a818-568a6bef1bc0}";
+                    meta = {
+                    };
+                  };
+                };
+              # Add the home-manager overlay to the Nixpkgs overlays.
+            }
+          )
 
           (final: prev: {
             vimPlugins =
--- nix/home-manager/configs/firefox.nix
+++ nix/home-manager/configs/firefox.nix
@@ -69,15 +69,7 @@ in {
           tampermonkey
           aw-watcher-web
           keepass-helper
-          (buildFirefoxXpiAddon rec {
-            pname = "carrot";
-            version = "0.6.8";
-            url = "https://addons.mozilla.org/firefox/downloads/file/4450645/carrot-${version}.xpi";
-            sha256 = "sha256-4QXp97JyqBBHcZbCReEzKCltTBc8WShETjVXYjq2WZ0";
-            addonId = "{f0eeb71a-e5d6-48e6-a818-568a6bef1bc0}";
-            meta = {
-            };
-          })
+          carrot
         ];
         settings = {
           # competitive-companion
