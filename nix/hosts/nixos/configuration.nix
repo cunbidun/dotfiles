5aa9dfbc0958b1211edb1f52604995b845e9d341
Duy Pham <cunbidun@gmail.com>
2025-06-06 00:46:20 -0400

    Add cachix for yazi (nixos version: system-1532)

--- nix/hosts/nixos/configuration.nix
+++ nix/hosts/nixos/configuration.nix
@@ -29,9 +29,18 @@
     };
     settings.trusted-users = ["root" "@wheel"];
     settings = {
-      substituters = ["https://hyprland.cachix.org"];
-      trusted-substituters = ["https://hyprland.cachix.org"];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://yazi.cachix.org"
+      ];
+      trusted-substituters = [
+        "https://hyprland.cachix.org"
+        "https://yazi.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+      ];
     };
   };
 
