ce19970113a9fd214f12c0f86c67ac0290791795
Duy Pham <cunbidun@gmail.com>
2025-08-02 12:45:31 -0400

    update lock

--- nix/home-manager/configs/hyprlock.nix
+++ nix/home-manager/configs/hyprlock.nix
@@ -1,6 +1,6 @@
 {
   pkgs,
-  inputs,
+  config,
   lib,
   ...
 }: {
@@ -11,28 +11,24 @@
         hide_cursor = true;
       };
 
-      background = lib.mkForce {
-        color = "rgba(25, 20, 20, 1.0)";
-        path = "screenshot";
-        blur_passes = 2;
+      background = {
+        path = lib.mkForce "screenshot";
+        blur_passes = 3;
+        blur_size = 5;
         brightness = 0.5;
       };
 
-      input-field = lib.mkForce [
-        {
-          size = "300, 50";
-          valign = "bottom";
-          position = "0%, 10%";
-          outline_thickness = 0;
-          fade_on_empty = true;
-          placeholder_text = "Enter Password";
-          dots_spacing = 0.1;
-          dots_center = true;
-          color = "rgb(202, 211, 245)";
-          check_color = "rgb(202, 211, 245)";
-          fail_color = "rgb(202, 211, 245)";
-        }
-      ];
+      input-field = {
+        size = "300, 50";
+        valign = "bottom";
+        position = "0%, 10%";
+        outline_thickness = 0;
+        fade_on_empty = true;
+        placeholder_text = "Enter Password";
+        dots_spacing = 0.1;
+        dots_center = true;
+        check_color = lib.mkForce "rgba(200, 200, 200, 1.0)";
+      };
 
       animations = {
         enabled = false;
@@ -42,7 +38,7 @@
         # Time
         {
           text = "cmd[update:1000] echo \"$(date +\"%-I:%M:%S %p\")\"";
-          color = "rgb(202, 211, 245)";
+          color = "rgba(200, 200, 200, 1.0)";
           font_size = 24;
           font_family = "SFMono Nerd Font";
           position = "0, 300";
@@ -52,7 +48,7 @@
         # Date
         {
           text = "cmd[update:43200000] echo \"$(date +\"%A, %B %e, %Y\")\"";
-          color = "rgb(202, 211, 245)";
+          color = "rgba(200, 200, 200, 1.0)";
           font_size = 16;
           font_family = "SFMono Nerd Font";
           position = "0, 200";
