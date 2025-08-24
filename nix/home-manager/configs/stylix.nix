d20f32d54bdc222107e57539eb10c0903bb93450
Duy Pham <cunbidun@gmail.com>
2025-08-24 15:03:34 -0400

    feat: add hyprpaper auto-restart systemd services for theme switching (version system-1822)

--- nix/home-manager/configs/stylix.nix
+++ nix/home-manager/configs/stylix.nix
@@ -242,4 +242,31 @@ in {
       extension = ".json";
     };
   };
+
+  # TODO: Check if we really need these 2 services
+  # One-shot service to restart hyprpaper
+  systemd.user.services.hyprpaper-restart = {
+    Unit = {
+      Description = "Restart hyprpaper service";
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.systemd}/bin/systemctl --user restart hyprpaper.service";
+    };
+  };
+
+  # Path watcher to restart hyprpaper when config changes
+  systemd.user.paths.hyprpaper-config-watcher = {
+    Unit = {
+      Description = "Watch hyprpaper config file for changes";
+    };
+    Path = {
+      PathModified = "%h/.config/hypr/hyprpaper.conf";
+      PathChanged = "%h/.config/hypr";
+      Unit = "hyprpaper-restart.service";
+    };
+    Install = {
+      WantedBy = ["graphical-session.target"];
+    };
+  };
 }
