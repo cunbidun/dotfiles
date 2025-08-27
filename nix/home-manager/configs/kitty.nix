2ccd40f6e56b13235ee63b63d8fbb2fa74432ac9
Duy Pham <cunbidun@gmail.com>
2025-08-26 23:20:13 -0400

    Update kitty

--- nix/home-manager/configs/kitty.nix
+++ nix/home-manager/configs/kitty.nix
@@ -1,9 +1,11 @@
-{...}: {
+{pkgs, ...}: {
   programs.kitty = {
     enable = true;
     keybindings = {
       "alt+v" = "paste_from_clipboard";
       "alt+c" = "copy_to_clipboard";
+      "alt+f" = "show_scrollback";
+      "alt+shift+f" = "launch --type=overlay --stdin-source=@screen_scrollback ${pkgs.fzf}/bin/fzf --no-sort --no-mouse --exact -i";
     };
     settings = {
       "confirm_os_window_close" = "0";
