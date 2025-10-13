c8a678f13ae6277488a073b5a8b7073a55c2ef8b
Duy Pham <cunbidun@gmail.com>
2025-10-13 00:38:31 -0400

    Auto commit: 2025-10-13 00:37:34 (nixos version: system-2077)

--- nix/home-manager/configs/chromium.nix
+++ nix/home-manager/configs/chromium.nix
@@ -6,21 +6,6 @@
   ...
 }: let
   inherit (pkgs.stdenv) isLinux;
-  mkChromiumProfile = {
-    name,
-    profileDir,
-  }: let
-    desktopName = lib.replaceStrings [" "] ["-"] (lib.toLower name);
-  in
-    lib.nameValuePair desktopName {
-      name = desktopName;
-      exec = "${pkgs.chromium}/bin/chromium --profile-directory=${profileDir}";
-      terminal = false;
-      categories = ["Network" "WebBrowser"];
-      startupNotify = true;
-      type = "Application";
-    };
-
   mkChromePWA = {
     name,
     url,
@@ -45,10 +30,6 @@ in {
   xdg = lib.mkIf isLinux {
     dataFile."icons/hicolor/scalable/apps/messenger.svg".source = "${project_root}/icons/messenger.svg";
     desktopEntries = lib.listToAttrs [
-      (mkChromiumProfile {
-        name = "Chromium Vi";
-        profileDir = "${config.home.homeDirectory}/.config/chromium/Chromium-Vi";
-      })
       (mkChromePWA {
         name = "Messenger";
         url = "https://www.messenger.com/";
