ad99e91ebd688235c7accf0c6e0db62599be467b
Duy Pham <cunbidun@gmail.com>
2025-10-27 22:23:37 -0400

    Auto commit: 2025-10-27 22:22:27 (nixos version: system-2136)

--- nix/home-manager/configs/nvchad.nix
+++ nix/home-manager/configs/nvchad.nix
@@ -20,20 +20,20 @@
       nodePackages.bash-language-server # Bash LS
       clang-tools # C/C++ LSP
       cargo # Rust
-      
+
       # Formatters
       stylua # Lua formatter
       black # Python formatter
       rustfmt # Rust formatter
       alejandra # Nix formatter
-      
+
       # Linters & Tools
       eslint # JavaScript/TypeScript linter
       shellcheck # Shell script linter
       ripgrep # Better grep
       fd # Better find
       fzf # Fuzzy finder
-      
+
       # Debugging
       lldb # LLDB debugger
       gdb # GDB debugger
@@ -75,6 +75,60 @@
 
       vim.diagnostic.config({ virtual_lines = true })
       vim.lsp.enable({ "luals", "nil_ls", "nixd", "pyright", "ruff", "bashls", "clangd" })
+
+      -- C/C++ Auto-formatting configuration
+      local conform = require("conform")
+      conform.setup({
+        formatters_by_ft = {
+          c = { "clang-format" },
+          cpp = { "clang-format" },
+          h = { "clang-format" },
+          hpp = { "clang-format" },
+        },
+        formatters = {
+          ["clang-format"] = {
+            command = "clang-format",
+            args = { "--style=file" },
+          },
+        },
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_fallback = true,
+        },
+      })
+
+      -- Copilot.lua configuration
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 75,
+          keymap = {
+            accept = "<M-l>",        -- Alt+L to accept suggestion
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",          -- Alt+] for next suggestion
+            prev = "<M-[>",          -- Alt+[ for prev suggestion
+            dismiss = "<C-]>",       -- Ctrl+] to dismiss
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ["."] = false,
+        },
+        copilot_node_command = "node",
+        server_opts_overrides = {},
+      })
+
+      -- Copilot-cmp integration for better completion menu
+      require("copilot_cmp").setup()
     '';
     backup = true;
     hm-activation = true;
