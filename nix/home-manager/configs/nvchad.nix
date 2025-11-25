ce3e8ce676aafc1e593e5c42cf0325cccc52524a
Duy Pham <cunbidun@gmail.com>
2025-11-25 17:17:05 -0500

    Auto commit: 2025-11-25 17:16:16 (nixos version: system-2217)

--- nix/home-manager/configs/nvchad.nix
+++ nix/home-manager/configs/nvchad.nix
@@ -57,6 +57,15 @@ in {
       M.base46 = {
         theme = "vscode_dark";
       }
+
+      -- Override lazy.nvim defaults to stop change detection notifications
+      M.lazy_nvim = {
+        change_detection = {
+          enabled = false,
+          notify = false,
+        },
+      }
+
       return M
     '';
 
