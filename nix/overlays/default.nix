29ab7757ba8b6f76424900205d6130eeb9e372b6
Duy Pham <cunbidun@gmail.com>
2025-11-30 01:03:55 -0500

    Auto commit: 2025-11-30 01:03:22 (nixos version: system-2229)

--- nix/overlays/default.nix
+++ nix/overlays/default.nix
@@ -4,11 +4,6 @@ in [
   (mkSubPkgsOverlay "nixpkgs-master" inputs.nixpkgs-master)
   (mkSubPkgsOverlay "nixpkgs-stable" inputs.nixpkgs-stable)
   inputs.nix4vscode.overlays.default
-  # Silence deprecated pkgs.system / stdenv.system alias (use stdenv.hostPlatform.system instead)
-  (_: super: {
-    system = super.stdenv.hostPlatform.system;
-    stdenv = super.stdenv // { system = super.stdenv.hostPlatform.system; };
-  })
   (import ./theme-manager.nix)
   (import ./everforest-themes.nix)
   (import ./mac-default-browser.nix inputs)
